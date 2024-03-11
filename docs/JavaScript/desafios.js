e50580fccc6adac76bb0a1d6d7ed0cf664f1dcd0
David Sierra <138825074+DavidSierra19@users.noreply.github.com>
2024-03-11 18:59:39 -0500

    Cambio Desafio

--- docs/JavaScript/desafios.js
+++ docs/JavaScript/desafios.js
@@ -1,9 +1,11 @@
 "use strict"
-//Hacer un programa que salude 
-/* console.log("Hola mundo")
-document.write("Hola mundo") */
+//Hacer un programa que envie un mensaje a la consola y a la interfaz de usuario
+/*
+console.log("Hola mundo")
+document.write("Hola mundo")
+*/
 
-// Hacer un progrma que salude por nombre, si no recibe nombre solo saludar
+// Hacer un progrma que reciba un nombre por parte del usuario, si exite nombre saludar por nombre de otra manera saludar Hola.
 /* 
 const nombreUsuario = prompt("Introdusca su nombre");
 
@@ -11,10 +13,11 @@ if(nombreUsuario){
     document.write("Hola ", nombreUsuario, "!")
 }else{
     document.write("Hola ")
-}  */
+}
+*/
 
-/* //-------------DESAFIO---------
-
+//HACER UN PROGRAMA QUE RECIBA UN NOMBRE POR PARTE DEL USUARIO, SI EXITE NOMBRE SALUDAR POR NOMBRE DE OTRA MANERA SALUDAR HOLA CON COMANDO SWITCH.
+/* 
 const Nombre = prompt("Inserte nombre")
 console.log(typeof Nombre)
 switch (Nombre){
@@ -24,32 +27,27 @@ switch (Nombre){
   default:
     console.log("Hola ")
     break;
-} */
-
-
-//-------HACER UN PROGRAMA QUE PIDA 2 NUMERO AL USURAIO Y LOS SUME------------
-
-/* 
-
-let Numero1 = prompt("Inserte Numero1");
-let Numero2 = prompt("Inserte Numero2");
-
-Numero1 = Number(Numero1);
-Numero2 = Number(Numero2);
-
-let Resultado = Numero1 + Numero2;
-
-document.write(Resultado) 
-
+} 
 */
 
-//---HACER UN PROGRAMA QUE PIDA APARTE NOMBRE Y APELLIDO Y MUESTRE EL NOMBRE COMPLETO 
+//---HACER UN PROGRAMA QUE PIDA APARTE NOMBRE Y APELLIDO Y CONTACATENE LOS DOS MOSTRANDO EL NOMBRE COMPLETO 
 /* 
 let Nombre = prompt("Ingrese su Nombre");
 let Apellido = prompt("Ingrese su Apellido");
-
 let Resultado = Nombre + " "+Apellido;
-
-document.write(Resultado)
-
+document.write(Nombre +" "+Apellido)
+// Interpolacion de variables
+document.write(`${Nombre} ${Apellido}`)
  */
+
+//-------HACER UN PROGRAMA QUE RECIBA 2 NUMERO AL USURAIO Y LOS SUME------------
+/* 
+let Numero1 = prompt("Inserte Numero1");
+let Numero2 = prompt("Inserte Numero2");
+Numero1 = Number(Numero1);
+Numero2 = Number(Numero2);
+let Resultado = Numero1 + Numero2;
+document.write(Resultado) 
+*/
+
+//
